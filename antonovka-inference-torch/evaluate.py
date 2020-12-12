746d13fadf95aec4acaee7953b31036545671569
Sergey_Zlobin <sirg_zlobin@mail.ru>
2020-12-13 00:03:56 +0300

    доп параметры

--- antonovka-inference-torch/evaluate.py
+++ antonovka-inference-torch/evaluate.py
@@ -1,12 +1,14 @@
 from src.model.inference import AppleClassification
 import ttach as tta
+import time
 import warnings
 
 warnings.filterwarnings("ignore")
 
-
 IMAGE_FOLDER = 'data/train_public/'
-MODEL_PATH = 'models/fold_0.ckpt'
+MODEL_PATH = './models/fold0.ckpt'
+OUTPUT_FILE = './Antonovka_out.csv'
+NUM_WORKERS = 4
 
 def process_folder():
     transforms = tta.Compose(
@@ -16,10 +18,15 @@ def process_folder():
             )
     system = AppleClassification(model_path=MODEL_PATH,
                                  device='cuda:0',
-                                 transforms=transforms)
-    _ = system.process_folder(IMAGE_FOLDER, csv_path='Antonovka_out.csv')
+                                 transforms=transforms,
+                                 th=0.2)
+    _ = system.process_folder(IMAGE_FOLDER, csv_path=OUTPUT_FILE, num_workers=NUM_WORKERS)
     return
 
-
 if __name__ == '__main__':
+    start_pred = time.time()
+
     process_folder()
+
+    end_pred = time.time()
+    print('Run time:', end_pred - start_pred, 'seconds')
