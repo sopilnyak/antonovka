67e5a9a07bddba65bcf0bf524cc1584bd7937da5
Olga Sopilnyak <o.sopilniak@gmail.com>
2020-12-12 12:34:43 +0300

    File upload

--- antonovka-api/app.py
+++ antonovka-api/app.py
@@ -1,9 +1,13 @@
 import os
+import uuid
+import mimetypes
+import io
 from celery.result import AsyncResult
 from flask import Flask, Blueprint, Response, flash, request, redirect, send_file, abort
 from flask_restplus import Api, Resource, fields
-from tasks import predict
+from tasks import predict, storage_folder
 from tasks import app as celery_app
+from parsers import file_upload
 
 app = Flask(__name__, static_url_path='/static')
 api = Api(version='1.0')
@@ -16,14 +20,27 @@ app.register_blueprint(blueprint)
 @api.route('/predict')
 class Predict(Resource):
     task_model = api.model('Task', {
-        'task_id': fields.String
+        'task_id': fields.String,
+        'filename': fields.String
     })
 
     @api.response(200, 'Task enqueued successfully', task_model)
-    def get(self):
-        print("start")
-        task = predict.apply_async(routing_key='predict')
-        return {'task_id': task.task_id}
+    @api.expect(file_upload)
+    def post(self):
+        if 'file' not in request.files:
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            return redirect(request.url)
+        filename = '{uuid}{extension}'.format(
+                uuid=str(uuid.uuid4()),
+                extension=os.path.splitext(file.filename)[1]
+            )
+        file.save(os.path.join(storage_folder, filename))
+
+        task = predict.apply_async(routing_key='predict',
+                                   kwargs={'filename': filename})
+        return {'task_id': task.task_id, 'filename': filename}
 
 
 @api.route('/<string:task_id>/status')
@@ -42,6 +59,17 @@ class Result(Resource):
             result = ''
         return {'status': task.status,
                 'result': result}
+
+
+@api.route('/image/<string:filename>')
+class Image(Resource):
+    def get(self, filename):
+        mimetypes.init()
+        with open(os.path.join(storage_folder, filename), 'rb') as file:
+            return send_file(
+                io.BytesIO(file.read()),
+                mimetype=mimetypes.types_map[os.path.splitext(filename)[1]][1:]
+            )
 
 
 @app.after_request
--- antonovka-api/tasks.py
+++ antonovka-api/tasks.py
@@ -11,10 +11,24 @@ app.conf.update(BROKER_URL=os.getenv('REDISTOGO_URL', 'redis://redis:6379/0'),
                 CELERY_RESULT_BACKEND=os.getenv('REDIS_URL', 'redis://redis:6379/0'),
                 BROKER_POOL_LIMIT=None)
 
+storage_folder = 'storage'
+if not os.path.exists(storage_folder):
+    os.mkdir(storage_folder)
 
-@app.task(name='predict')
-def predict():
+
+class PredictTask(celery.Task):
+    model_object = None
+
+    @property
+    def model(self):
+        if self.model_object is None:
+            self.model_object = 'model'
+        return self.model_object
+
+
+@app.task(base=PredictTask, name='predict')
+def predict(filename):
     print('starting predict')
     time.sleep(5)
     print('ending predict')
-    return 'result'
+    return filename
